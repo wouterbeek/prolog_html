417d5c9e1a638b8b37ea287319dad65f85cbc9f6
Wouter Beek <wouter@triply.cc>
2017-05-21 11:52:53 +0200

    Added requirements for a simple RDF browser in Pack `rdf_client'.
    ADDED: ellipsis//2, flag_icon//1, html_date_time//[1,2], icon//1,
           link//[1,2], mail_icon//1 @html_ext

--- prolog/html/html_ext.pl
+++ prolog/html/html_ext.pl
@@ -1,24 +1,33 @@
 :- module(
   html_ext,
   [
-    deck//2,              % :Card_1, +Items
-    deck//3,              % +Attrs, :Card_1, +Items
-    html_call//1,         % :Html_0
-    html_call//2,         % :Html_1, +Arg1
-    html_maplist//2,      % :Html_1, +Args1
-    html_thousands//1,    % +Integer
-    image//1,             % +Spec
-    image//2,             % +Spec, +Attrs
+    deck//2,               % :Card_1, +Items
+    deck//3,               % +Attrs, :Card_1, +Items
+    ellipsis//2,           % +Str, +MaxLen
+    external_link_icon//1, % +Uri
+    flag_icon//1,          % +LTag
+    html_call//1,          % :Html_0
+    html_call//2,          % :Html_1, +Arg1
+    html_date_time//1,     % +Something
+    html_date_time//2,     % +Something, +Opts
+    html_maplist//2,       % :Html_1, +Args1
+    html_thousands//1,     % +Integer
+    image//1,              % +Spec
+    image//2,              % +Spec, +Attrs
+    link//1,               % +Pair
+    link//2,               % +Attrs, +Pair
+    mail_icon//1,          % +Uri
     meta_authors//0,
-    meta_description//1,  % +Desc
-    navbar//3,            % :Brand_0, :Menu_0, :Right_0
-    open_graph//2,        % +Key, +Value
-    tooltip//2,           % +String, :Html_0
+    meta_description//1,   % +Desc
+    navbar//3,             % :Brand_0, :Menu_0, :Right_0
+    open_graph//2,         % +Key, +Value
+    tooltip//2,            % +String, :Html_0
     twitter_follow_img//0
   ]
 ).
 :- reexport(library(http/html_head)).
 :- reexport(library(http/html_write)).
+:- reexport(library(http/js_write)).
 
 /** <module> HTML extensions
 
@@ -30,18 +39,21 @@ html({|html||...|}).
 ```
 
 @author Wouter Beek
-@version 2017/04
+@version 2017/04-2017/05
 */
 
 :- use_module(library(apply)).
+:- use_module(library(date_time)).
 :- use_module(library(debug)).
+:- use_module(library(html/html_date_time_human)).
+:- use_module(library(html/html_date_time_machine)).
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_path)).
 :- use_module(library(http/http_resource)).
 :- use_module(library(http/jquery)).
 :- use_module(library(lists)).
 :- use_module(library(settings)).
-:- use_module(library(uri_ext)).
+:- use_module(library(uri/uri_ext)).
 
 % jQuery
 :- set_setting(jquery:version, '3.2.1.min').
@@ -153,12 +165,6 @@ nlp:nlp_string0(nl, follow_us_on_x, "Volg ons op ~s").
      _,
      "Optional Twitter profile name."
    ).
-:- setting(
-     nlp:language,
-     atom,
-     en,
-     "The natural language in which content should be displayed to the user."
-  ).
 
 
 
@@ -177,6 +183,41 @@ deck(Attrs1, Card_1, L) -->
 
 
 
+%! ellipsis(+Str, +MaxLen)// is det.
+
+ellipsis(Str, MaxLen) -->
+  {string_ellipsis(Str, MaxLen, Ellipsis)},
+  ({Str == Ellipsis} -> html(Str) ; tooltip(Str, Ellipsis)).
+
+
+
+%! external_link_icon(+Uri)// is det.
+
+external_link_icon(Uri) -->
+  html(a([href=Uri,target='_blank'], \icon(external_link))).
+
+
+
+%! flag_icon(+LTag)// is det.
+
+flag_icon(LTag) -->
+  {
+    file_name_extension(LTag, svg, File),
+    directory_file_path(flag_4x3, File, Path)
+  },
+  html(span(class=[label,'label-primary'], [\flag_icon_img(Path)," ",LTag])).
+
+
+flag_icon_img(Path) -->
+  {
+    absolute_file_name(img(Path), _, [access(read)]), !,
+    http_absolute_location(img(Path), Location)
+  },
+  html(span(class='flag-icon', img(src=Location))).
+flag_icon_img(_) --> [].
+
+
+
 %! html_call(:Html_0)// is det.
 %! html_call(:Html_1, +Arg1)// is det.
 
@@ -186,6 +227,45 @@ html_call(Html_0, X, Y) :-
 
 html_call(Html_1, Arg1, X, Y) :-
   call(Html_1, Arg1, X, Y).
+
+
+
+%! html_date_time(+Something)// is det.
+%! html_date_time(+Something, +Opts)// is det.
+%
+% Generates human- and machine-readable HTML for date/times.
+%
+% The following options are supported:
+%
+%   * ltag(+oneof([en,nl])
+%
+%     The language tag denoting the natural language that is used to
+%     display human-readable content in.  The default is `en`.
+%
+%   * masks(+list(atom))
+%
+%     The following masks are supported: `none`, `year`, `month`,
+%     `day`, `hour`, `minute`, `second`, `offset`.  The default is
+%     `[]`.
+%
+%   * month_abbr(+boolean)
+%
+%     Whether the human-readable representation of month names should
+%     use abbreviated names or not.  The default is `false`.
+
+html_date_time(Something) -->
+  {current_ltag(LTag)}, !,
+  html_date_time(Something, _{ltag: LTag}).
+
+
+html_date_time(Something, Opts) -->
+  {
+    something_to_date_time(Something, DT),
+    html_machine_date_time(DT, MachineString),
+    get_dict(masks, Opts, [], Masks),
+    date_time_masks(Masks, DT, MaskedDT)
+  },
+  html(time(datetime=MachineString, \html_human_date_time(MaskedDT, Opts))).
 
 
 
@@ -206,6 +286,57 @@ html_thousands(Integer) -->
 
 
 
+%! icon(+Name)// is det.
+
+% @tbd Use file `img/pen.svg' instead.
+icon(pen) --> !,
+  html(
+    svg([width=14,height=14,viewBox=[0,0,300,300]],
+      path([fill='#777777',d='M253 123l-77-77 46-46 77 77-46 46zm-92-61l77 77s-35 16-46 77c-62 62-123 62-123 62s-24 36-46 15l93-94c55 12 50-39 37-52s-62-21-52 37L7 277c-21-21 15-46 15-46s0-62 62-123c51-5 77-46 77-46z'], [])
+    )
+  ).
+icon(Name) -->
+  {icon_class(Name, Class)},
+  html(span(class([fa,Class]), [])).
+
+
+
+%! icon_class(+Name, -Class, -Title) is det.
+
+icon_class(Name, Class) :-
+  icon_class_title(Name, Class, _).
+
+
+
+%! icon_class_title(+Name, -Class, -Title) is det.
+
+icon_class_title(Name, Class, Title) :-
+  icon_table(Name, ClassPostfix, Title),
+  atomic_list_concat([fa,ClassPostfix], -, Class).
+
+
+
+%! icon_table(?Name, ?Class, ?Title) is nondet.
+
+% CRUD = Create, Read, Update, Delete.
+icon_table(cancel,         eraser,          "Cancel").
+icon_table(copy,           copy,            "Copy").
+icon_table(create,         pencil,          "Create").
+icon_table(delete,         trash,           "Delete").
+icon_table(download,       download,        "Download").
+icon_table(external_link,  'external-link', "Follow link").
+icon_table(internal_link,  link,            "Follow link").
+icon_table(mail,           envelope,        "Send email").
+icon_table(tag,            tag,             "").
+icon_table(tags,           tags,            "").
+icon_table(time,           'clock-o',       "Date/time").
+icon_table(user,           user,            "Log me in").
+icon_table(vote_down,     'thumbs-o-down',  "Vote up").
+icon_table(vote_up,       'thumbs-o-up',    "Vote down").
+icon_table(web,            globe,           "Visit Web site").
+
+
+
 %! image(+Spec)// is det.
 %! image(+Spec, +Attrs)// is det.
 
@@ -219,6 +350,31 @@ image(Spec, Attrs1) -->
     merge_attrs(Attrs1, [src=Uri], Attrs2)
   },
   html(img(Attrs2, [])).
+
+
+
+%! link(+Pair)// is det.
+%! link(+Attrs, +Pair)// is det.
+%
+% Pair is of the form `Rel-Uri`, where Uri is based on Spec.
+
+link(Pair) -->
+  link([], Pair).
+
+
+link(Attrs1, Rel-Spec) -->
+  {
+    uri_specification(Spec, Uri),
+    merge_attrs(Attrs1, [href=Uri,rel=Rel], Attrs2)
+  },
+  html(link(Attrs2, [])).
+
+
+
+%! mail_icon(+Uri)// is det.
+
+mail_icon(Uri) -->
+  external_link(Uri, [property='foaf:mbox'], [" ",\icon(mail)]).
 
 
 
@@ -303,11 +459,6 @@ twitter_follow_img -->
   {nlp_string(follow_us_on_x, ["Twitter"], String)},
   tooltip(String, \twitter_follow0(User, \twitter_img0)).  
 twitter_follow_img --> [].
-
-nlp_string(Name, Args, String) :-
-  setting(nlp:language, LTag),
-  nlp:nlp_string0(LTag, Name, Format),
-  format(string(String), Format, Args).
 
 twitter_follow0(User, Html_0) -->
   {twitter_user_uri0(User, Uri)},
